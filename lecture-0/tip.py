3089586c33729e6791202676ee53c9669c2c47ac
guywholikestocode2 <130280646+guywholikestocode2@users.noreply.github.com>
2023-04-09 16:02:13 +0530

    Update tip.py

--- lecture-0/tip.py
+++ lecture-0/tip.py
@@ -1,16 +1,16 @@
 def main():
-    dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
+    dollars = dollars_to_float(input("How much was the meal? ")) # Asking the user the cost of the meal
+    percent = percent_to_float(input("What percentage would you like to tip? ")) # Asking the user the percentage of the cost they would like to tip
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-
+#Function for converting dollars from str to float
 def dollars_to_float(d):
     d = d.replace("$", "")
     d = float(d)
     return d
 
-
+#Function for converting the percentage from str to float
 def percent_to_float(p):
     p = p.replace("%", "")
     p = float(p)
