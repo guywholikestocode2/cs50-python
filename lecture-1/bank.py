0a8780f285d6b1e18d7f6472f59f3c3142ec8a61
guywholikestocode2 <130280646+guywholikestocode2@users.noreply.github.com>
2023-05-03 18:00:30 +0530

    Update bank.py

--- lecture-1/bank.py
+++ lecture-1/bank.py
@@ -1,1 +1,9 @@
-
+def main():
+   g = input("Greeting: ").lower().strip()
+   if g.startswith("hello"):
+      print("$0")
+   elif g.startswith("h"):
+      print("$20")
+   else:
+      print("$100")
+main ()
