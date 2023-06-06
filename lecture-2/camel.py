eff882392553f06532c7e79254a31d781234f72f
guywholikestocode2 <130280646+guywholikestocode2@users.noreply.github.com>
2023-06-06 19:20:51 +0530

    Add files via upload

--- lecture-2/camel.py
+++ lecture-2/camel.py
@@ -1,1 +1,22 @@
+def main():
+    camel = input("camelCase: ")
+    snake_case(camel)
 
+
+
+
+
+def snake_case(camel):
+    snake_string = ""
+    for i in range(len(camel)):
+        if camel[i].isupper():
+         snake_string += f"_{camel[i].lower()}"
+        else:
+         snake_string += camel[i]
+
+    print("snake_case:", snake_string)
+
+
+
+
+main()
