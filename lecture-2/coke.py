34be94ab6e136b0820db9cd3a82f359c923883be
guywholikestocode2 <130280646+guywholikestocode2@users.noreply.github.com>
2023-06-07 10:13:19 +0530

    Update coke.py

--- lecture-2/coke.py
+++ lecture-2/coke.py
@@ -13,12 +13,15 @@ def due(y):
     hell = int(input("Insert Coin: "))
     a = y - hell
     f = a * (-1)
-    if a > 0:
-        print("Amount Due: ", a)
+    acc = [25, 10, 5]
+    if a > 0 and hell not in acc:
+        print("Amount Due:", y)
+        return y
+    elif a > 0 and hell in acc:
+        print("Amount Due:", a)
+        return a
     elif a <= 0:
-        print("Change Owed: ", f)
-    return a
-
+        print("Change Owed:", f)
 
 
 main()
